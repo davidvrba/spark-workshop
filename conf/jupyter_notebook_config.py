c3dff17c2a60b5142813780b6b856cb7149e6657
vrbad <david.vrba@emplifi.io>
2023-06-12 16:34:53 +0200

    port for jupyter notebook changed to 80

--- conf/jupyter_notebook_config.py
+++ conf/jupyter_notebook_config.py
@@ -10,4 +10,4 @@ c.NotebookApp.ip = '*'
 c.NotebookApp.allow_remote_access = True
 c.NotebookApp.open_browser = False
 c.NotebookApp.password = u'argon2:$argon2id$v=19$m=10240,t=10,p=8$u//Q8MyDuFWl2MR/ykjQ2Q$1nPQDAwexooNoE1d5+3GqPEYz6oxvM9mnEIgu2hZRmc'
-c.NotebookApp.port = 8888
+c.NotebookApp.port = 80
